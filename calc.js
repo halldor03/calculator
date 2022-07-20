ce6d5b852490ffb57867e84f620a45bdef92c8fd
halldor03 <kacoleker@gmail.com>
2022-07-20 23:59:30 +0200

    Trying different approach

--- calc.js
+++ calc.js
@@ -1,11 +1,20 @@
-const operands = {
-  currentNumber: 0,
-  firstNumber: 0,
-  nextNumber: 0,
+const mem = {
+  currentNumber: null,
+  firstNumber: null,
+  nextNumber: null,
+  operator: null,
 };
-let currentNumber = operands.currentNumber;
-let firstNumber = operands.firstNumber;
-let nextNumber = operands.nextNumber;
+let currentNumber = mem.currentNumber;
+let firstNumber = mem.firstNumber;
+let nextNumber = mem.nextNumber;
+let operator = mem.operator;
+
+const display = document.querySelector("#calculatorDisplay");
+const numericKeys = document.querySelectorAll("div#numericKeys>button");
+const functionKeys = document.querySelectorAll("div#functionKeys>button");
+const operationKeys = document.querySelectorAll("div#operationKeys>button");
+const decimalButton = document.querySelector(".decimalPointKey");
+const equalsButton = document.querySelector(".equalsKey");
 
 const add = function (firstN, nextN) {
   let result = firstNumber + nextNumber;
@@ -27,79 +36,78 @@ const divide = function (firstN, nextN) {
   return result;
 };
 
-const populateDisplay = function (key) {
-  let keyPushed = key.path[0].outerText;
-  let outputNumber = (display.textContent += keyPushed);
-  if (outputNumber.length === 9) {
-    numericKeys.forEach((key) =>
-      key.removeEventListener("click", populateDisplay)
-    );
-  }
-  if (keyPushed === ".") {
-    decimalButton.removeEventListener("click", populateDisplay);
-  }
-  currentNumber = Number(outputNumber);
-  return currentNumber;
-};
-const display = document.querySelector("#calculatorDisplay");
-const decimalButton = document.querySelector(".decimalPointKey");
-const numericKeys = document.querySelectorAll("div#numericKeys>button");
-numericKeys.forEach((key) => key.addEventListener("click", populateDisplay));
-
 const calculatorFunctions = function (key) {
   let keyPushed = key.path[0].outerText;
   if (keyPushed === "AC") {
-    display.textContent = "";
-    numericKeys.forEach((key) =>
-      key.addEventListener("click", populateDisplay)
-    );
+    firstNumber = 0;
+    nextNumber = 0;
+    display.innerText = "";
   }
   // if (keyPushed === "C") {
   // }
   // if (keyPushed === "\xB1") {
   // }
 };
-const functionKeys = document.querySelectorAll("div#functionKeys>button");
 functionKeys.forEach((key) =>
   key.addEventListener("click", calculatorFunctions)
 );
 
-const operate = function (key, firstN, nextN) {
+const populateDisplay = function () {
+  for (let i = 0; i < numericKeys.length; i++) {
+    numericKeys[i].addEventListener("click", () => {
+      let keyPushed = numericKeys[i].value;
+      let outputNumber = (display.innerText += keyPushed);
+      if (outputNumber.length > 9) {
+        display.innerText = outputNumber.substring(0, 9);
+      }
+      if (outputNumber.includes(".")) {
+        decimalButton.disabled = true;
+      }
+      currentNumber = parseFloat(outputNumber);
+      return currentNumber;
+    });
+  }
+};
+
+const inputNumber = function () {
+  if (display.innerText === "") {
+    populateDisplay();
+  }
+};
+inputNumber();
+
+const inputOperator = function (key) {
   let keyPushed = key.path[0].outerText;
   switch (true) {
     case keyPushed === "+":
-      numericKeys.forEach((key) =>
-        key.addEventListener("click", populateDisplay)
-      );
+      decimalButton.disabled = false;
       firstNumber = currentNumber;
-      console.log("");
-      console.log("Current number: " + currentNumber);
-      console.log("First number: " + firstNumber);
-      console.log("Second number: " + nextNumber);
-      break;
-    case keyPushed === "\u2212":
-      console.log("Minus");
-      console.log(substract(firstNumber, nextNumber));
-      break;
-    case keyPushed === "\xD7":
-      console.log("Times");
-      console.log(multiply(firstNumber, nextNumber));
-      break;
-    case keyPushed === "\xF7":
-      console.log("Divided");
-      console.log(divide(firstNumber, nextNumber));
-      break;
-    case keyPushed === "=":
-      nextNumber = currentNumber;
-      let result = add(firstNumber, nextNumber);
-      display.textContent = result;
-      result = currentNumber;
-      console.log("Current number: " + currentNumber);
-      console.log("First number: " + firstNumber);
-      console.log("Second number: " + nextNumber);
-      console.log("");
-      break;
+      display.innerText = "";
+      for (let i = 0; i < numericKeys.length; i++) {
+        numericKeys[i].addEventListener("click", () => {
+          nextNumber = parseFloat(display.innerText);
+          console.log("First number: " + firstNumber);
+          console.log("Second number: " + nextNumber);
+          return nextNumber;
+        });
+      }
+  }
+  return (operator = "+");
+};
+operationKeys.forEach((key) => key.addEventListener("click", inputOperator));
+
+const operate = function (op) {
+  console.log("First number: " + firstNumber);
+  console.log("Second number: " + nextNumber);
+  switch (true) {
+    case operator === "+":
+      return console.log("Sum: " + add(firstNumber, nextNumber));
+    case "-":
+      return substract(firstNumber, nextNumber);
+    case "*":
+      return multiply(firstNumber, nextNumber);
+    case "/":
+      return divide(firstNumber, nextNumber);
   }
 };
-const operationKeys = document.querySelectorAll("div#operationKeys>button");
-operationKeys.forEach((key) => key.addEventListener("click", operate));
+equalsButton.addEventListener("click", operate);
